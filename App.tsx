1d8917688cb3db20582c0a70a79ea78291fcc4c7
Nicolas Tardieu <ntardieu.contact@gmail.com>
2023-10-01 21:51:13 +0200

    feat(App): enable feature VerticalProgressBar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import VerticalProgressBar from './components/ui/VerticalProgressBar/VerticalProgressBar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <VerticalProgressBar fillDuration={5000}></VerticalProgressBar>
       <StatusBar style="auto" />
     </View>
   );
