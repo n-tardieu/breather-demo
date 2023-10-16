bebf52bd14f7b37e8c07587bf4f992c0c1997256
Nicolas Tardieu <ntardieu.contact@gmail.com>
2023-10-17 00:21:01 +0200

    init TimeProgressBar component

---
+++ components/ui/TimeProgressBar /TimeProgressBar.style.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#F2F2F0',
+    borderRadius: 6,
+    borderColor: '#555938',
+    alignItems: 'flex-start',
+    position: 'absolute',
+    width: 250,
+    bottom: 7,
+  },
+  bar: {
+    backgroundColor: '#5A6E73',
+    borderRadius: 20,
+    shadowColor: 'lightblue',
+    shadowOffset: { width: 0, height: 0 },
+  },
+  fillDuration : {
+
+  }
+});
---
+++ components/ui/TimeProgressBar /TimeProgressBar.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { Easing, View } from 'react-native';
+import styles from './TimeProgressBar.style';
+import { Animated } from 'react-native';
+
+interface TimeProgressBarProps {
+    isPlaying: boolean;
+    fillDuration: number;
+}
+
+const TimeProgressBar: React.FC<TimeProgressBarProps> = ({ isPlaying, fillDuration }) => {
+    const [progress] = useState(new Animated.Value(5));
+
+    useEffect(() => {
+        if (isPlaying)
+            timerLogic()
+        else
+            pauseAnimation()
+    }, [isPlaying])
+
+    const timerLogic = () => {
+        Animated.timing(progress, {
+            toValue: 250,
+            duration: (fillDuration),
+            useNativeDriver: false
+        }).start()
+    }
+
+    const pauseAnimation = () => {
+        progress.stopAnimation()
+    };
+
+    const containerStyle = {
+        height: 5,
+        backgroundColor: 'grey',
+        margin: 10,
+    };
+
+    const barStyle = {
+        height: 5,
+        backgroundColor: '#333',
+    };
+
+
+    return (
+        <View style={[styles.container, containerStyle]}>
+            <Animated.View style={[styles.bar, barStyle, { width: progress }]} />
+        </View>
+    );
+};
+
+export default TimeProgressBar;
