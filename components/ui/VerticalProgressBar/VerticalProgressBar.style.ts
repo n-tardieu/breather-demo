4aa304d3664cb8b1ea37e8a49dd7b7e9346bdb57
Nicolas Tardieu <ntardieu.contact@gmail.com>
2023-11-03 12:35:26 +0100

    improvement(VerticalProgressBar): container style

--- components/ui/VerticalProgressBar/VerticalProgressBar.style.ts
+++ components/ui/VerticalProgressBar/VerticalProgressBar.style.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     backgroundColor: '#F2F2F0',
-    borderRadius: 6,
+    borderRadius: 10,
     borderWidth: 2,
     borderColor: '#555938',
     alignItems: 'center',
