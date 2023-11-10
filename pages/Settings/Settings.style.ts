8a219f005331a09a8c6bcb1feebd83d25e2244d8
Nicolas Tardieu <ntardieu.contact@gmail.com>
2023-11-10 23:23:48 +0100

    improvement(Settings): update settings

---
+++ pages/Settings/Settings.style.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 40
+    },
+    header: {
+        flexDirection: 'row',
+        height: 50,
+        alignItems: 'center'
+    },
+    section: {
+        marginLeft: 20,
+        marginBottom: 20,
+    },
+    title: {
+        paddingTop: 10
+    },
+    param: {
+        paddingTop: 10
+    },
+    subparam: {
+        color: 'green'
+    },
+    version: {
+        marginLeft: 20,
+        marginBottom: 20,
+    }
+});
--- pages/Settings/Settings.tsx
+++ pages/Settings/Settings.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { StackHeaderProps } from '@react-navigation/stack';
 import BackButton from '../../features/BackButton/BackButton';
-
+import styles from './Settings.style'
 
 const Settings: React.FC<StackHeaderProps> = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <BackButton onPress={navigation.goBack}></BackButton>
-      <Text>Currently nothing is configurable</Text>
-      <Text>I'm listening to your suggestions</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <BackButton onPress={navigation.goBack}></BackButton>
+        <Text>Réglages</Text>
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.param}>Durée total</Text>
+        <Text style={styles.subparam}>5min</Text>
+
+        <Text style={styles.param}>Durée d'inspiration</Text>
+        <Text style={styles.subparam}>5s</Text>
+        <Text style={styles.param}>Durée d'expiration</Text>
+        <Text style={styles.subparam}>5s</Text>
+      </View>
+
+      <View style={styles.version}>
+        <Text style={styles.param}>Version 1.0.0_220324</Text>
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.title}>A propos</Text>
+      </View>
     </View>
   );
 }
